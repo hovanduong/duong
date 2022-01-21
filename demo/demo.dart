155d6d66ca8efdfb2ba7c7a0768d00c3a1c897f2
hovanduong <52810802+hovanduong@users.noreply.github.com>
2022-01-21 13:21:39 +0700

    update

--- demo/demo.dart
+++ demo/demo.dart
@@ -1,2 +1,11 @@
 // chuc nang 1 
 // chuc nang 2
+// chuc nang 4
+// Nhieu nguoi lam tren mot file = > file=> ra thanh nhieu file=> file 1, flie 2,file 3
+// chuong trinh tinh  tru, nhan, chia 
+/*
+File: Noi chay chuong trinh: main
+file1: chuc nang tinh tru,
+..
+..
+*/
--- demo/demo1.dart
+++ demo/demo1.dart
@@ -1,1 +1,2 @@
 // chuc nang demo1
+// chuc nang 2
